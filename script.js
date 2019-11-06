bed7bc1ee45e8ff56879c78bad54fff35d6b6df9
Zadymejro <36375892+ToxicPL@users.noreply.github.com>
2019-11-06 16:56:31 +0100

    added search button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*==================Actual time from the computer==================*/
 function time() {
   var today = new Date();
   var hours = today.getHours();
@@ -15,3 +16,24 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
+/*======================================================*/
+/*==================Search==================*/
+var defaultSearch = "http://google.com/search?q=";
+var input = document.querySelector(".inp");
+input.addEventListener("keydown", e => {
+  var keycode = e.keyCode;
+  if (keycode == 13) {
+    function search() {
+      window.open(`${defaultSearch}${q}`);
+    }
+
+    if (input.value > 0) {
+      input.addEventListener("input", search, false);
+    }
+
+    setTimeout(() => {
+      input.value = "";
+    }, 1);
+  }
+});
+/*======================================================*/
